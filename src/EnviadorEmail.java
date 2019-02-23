1987c8538665b8a80ceac006f9e25691e7bca69c
Igor Herrera <igorherrerasantos@gmail.com>
2019-02-23 16:18:15 -0300

    add de enviador de email

--- src/EnviadorEmail.java
+++ src/EnviadorEmail.java
@@ -1,7 +1,13 @@
 
-public class EnviadorEmail {
+public class EnviadorEmail implements AcaoAposGerarNota {
 
 	public void enviarEmail(NotaFiscal nf) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void executa(NotaFiscal nf) {
 		// TODO Auto-generated method stub
 		
 	}
